d44f5960142c0df5bcf7171a04e12252084b328a
akihiro noda <aki.ap11@gmail.com>
2021-09-12 20:11:00 +0900

    feat: Add login page.

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-app-bar-title>
+        {{title}}
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn v-if="this.$auth.loggedIn" outlined @click="logout">
+        Sing out
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'App'
+    }
+  },
+  methods: {
+    logout () {
+      this.$auth.logout()
+    }
+  }
+}
+</script>
